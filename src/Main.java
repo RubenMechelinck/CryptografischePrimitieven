7ca3419ff322bb8d10ee0222c5a77da699174eb9
Pieter-Jan Vanhaverbeke <pieter-jan.vanhaverbeke@student.kuleuven.be>
2018-12-13 14:33:24 +0100

    naar bytearray

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import org.apache.commons.lang.SerializationUtils;
+
 public class Main {
     public static void main(String[] args){
         Person person1 = new Person("Pieter","Roeselare","000");
         Person person2 = new Person("Jan","Owakdoukou","11/09/2001");
         Person person1Fake = new Person("Peeter","Roeselare", "000");
 
+        //Naar bytearray
+
+        byte[] p1 = naarByteArray(person1);
+        byte[] p2 = naarByteArray(person2);
+        byte[] pFake = naarByteArray(person1Fake);
+
     }
 
 
 
 
-    public void NaarByteArray(Person person){
-        
+    public static byte[] naarByteArray(Person person){
+        return SerializationUtils.serialize(person);
     }
 
 }
