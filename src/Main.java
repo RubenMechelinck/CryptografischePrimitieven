613cb7a5e8e2a9ca962a916eaecffdf683129f9c
Ruben Mechelinck <ruben.mechelinck@hotmail.be>
2018-12-13 14:14:10 +0100

    initial duw

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Person person1 = new Person("Pieter","Roeselare","000");
+        Person person2 = new Person("Jan","Owakdoukou","11/09/2001");
+        Person person1Fake = new Person("Peeter","Roeselare", "000");
+
+    }
+}
